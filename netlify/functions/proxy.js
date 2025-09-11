11c625f2eaeb0ce58831562c051ba1651dd9a9bd
itjim <44407043+itjim@users.noreply.github.com>
2025-09-11 08:39:02 -0500

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -8,9 +8,16 @@ export async function handler(event, context) {
   }
 
   try {
-    const response = await fetch(target);
-    const text = await response.text();
+    const response = await fetch(target, {
+      headers: {
+        "accept": "application/json, text/plain, */*",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+        "origin": "http://bgyfl-website-new-prod.s3-website.us-east-2.amazonaws.com",
+        "referer": "http://bgyfl-website-new-prod.s3-website.us-east-2.amazonaws.com/"
+      }
+    });
 
+    const text = await response.text();
     return {
       statusCode: 200,
       headers: {
